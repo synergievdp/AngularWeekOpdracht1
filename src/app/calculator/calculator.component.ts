25d87e39de9316eed7ad0ff1b2d606642f7cfcd7
Synergie van der Pluijm <synergievdp@gmail.com>
2021-09-08 15:42:16 +0200

    buttons

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
-
+  buttons: number[] = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+  operators: string[] = ["x", "/", "+", "-"]
   constructor() { }
 
   ngOnInit(): void {
